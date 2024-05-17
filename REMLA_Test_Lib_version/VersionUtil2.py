5c0d0fa3d83d46518ed98889c054c9f673bec01a
nickdubbel <nick.dubbeldam@live.nl>
2024-05-17 11:48:15 +0200

    Change folder name

--- REMLA_Test_Lib_version/VersionUtil2.py
+++ REMLA_Test_Lib_version/VersionUtil2.py
@@ -1,4 +1,4 @@
-from REMLA-Test-Lib-version.version import VersionUtil
+from REMLA_Test_Lib_version import VersionUtil
 
 version = VersionUtil.get_version()
 print(f"The current library version is: {version}")
